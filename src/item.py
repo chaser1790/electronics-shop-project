38697af0197e0c5f2490b6501b46c6d5bd256c5a
chaser-omg <zolagalaxy1790@gmail.com>
2023-09-23 20:01:41 +0300

    доработал, вернул код из предыдущего задания.

--- src/item.py
+++ src/item.py
@@ -1,25 +1,63 @@
 import csv
 
 class Item:
+    """Класс, представляющий товар."""
+
+    pay_rate = 1  # уровень цен с учетом скидки
+    all_items = []  # список всех созданных объектов класса Item
+
     def __init__(self, name, price, quantity):
+        """
+        Инициализация объекта класса Item.
+        :param name: Название товара (str)
+        :param price: Цена товара (float)
+        :param quantity: Количество товара (int)
+        """
         self._name = name
-        self.price = price
-        self.quantity = quantity
+        self.price = round(float(price), 2)
+        self.quantity = int(quantity)
+        Item.all_items.append(self)
 
     @property
     def name(self):
+        """Свойство для получения названия товара."""
         return self._name
 
     @name.setter
     def name(self, value):
+        """
+        Свойство для установки названия товара с проверкой на максимальное
+        количество символов (10).
+        :param value: Новое название товара (str)
+        """
         if len(value) <= 10:
             self._name = value
         else:
             self._name = value[:10]
             raise Exception("Длина наименования товара превышает 10 символов.")
 
+    def calculate_total_price(self, pay_rate):
+        """
+        Рассчитать общую стоимость товара с учетом количества и уровня цен.
+        :param pay_rate: Уровень цен с учетом скидки (float)
+        :return: Общая стоимость товара (float)
+        """
+        return self.price * self.quantity * pay_rate
+
+    def apply_discount(self, pay_rate):
+        """
+        Применить скидку к товару, обновляя его стоимость.
+        :param pay_rate: Уровень цен с учетом скидки (float)
+        """
+        self.price = self.price * pay_rate
+        self.price = round(self.price, 2)
+
     @classmethod
     def instantiate_from_csv(cls, file_path):
+        """
+        Создание объектов класса Item на основе данных из csv-файла.
+        :param file_path: Путь к csv-файлу (str)
+        """
         items = []
         with open(file_path, newline='') as csvfile:
             reader = csv.DictReader(csvfile)
@@ -33,4 +71,9 @@ class Item:
 
     @staticmethod
     def string_to_number(string):
+        """
+        Преобразование числа в виде строки в целочисленное значение.
+        :param string: Число в виде строки (str)
+        :return: Число в виде целого числа (int)
+        """
         return int(float(string))
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,23 +1,80 @@
 import pytest
 from src.item import Item
 
-def test_name_setter():
-    item = Item('Телефон', 10000, 5)
+class TestItem:
+    """Набор тестов для класса Item."""
 
-    # длина наименования товара меньше 10 символов
-    item.name = 'Смартфон'
-    assert item.name == 'Смартфон'
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        Item.all_items = []
+        Item.pay_rate = 1
 
-    # длина наименования товара больше 10 символов
-    with pytest.raises(Exception) as excinfo:
-        item.name = 'СуперСмартфон'
-    assert "Длина наименования товара превышает 10 символов." in str(excinfo.value)
+    def test_init(self):
+        """
+        Тестирование правильности создания экземпляра класса Item.
+        """
+        item = Item("Смартфон", 10000, 20)
+        assert item.name == "Смартфон"
+        assert item.price == 10000
+        assert item.quantity == 20
+        assert item in Item.all_items
 
-def test_instantiate_from_csv():
-    Item.instantiate_from_csv('src/items.csv')
-    assert len(Item.all) == 5
+    def test_calculate_total_price(self):
+        """
+        Тестирование метода calculate_total_price.
+        """
+        item = Item("Смартфон", 10000, 20)
+        total_price = item.calculate_total_price(pay_rate=1)
+        assert total_price == 200000
 
-def test_string_to_number():
-    assert Item.string_to_number('5') == 5
-    assert Item.string_to_number('5.0') == 5
-    assert Item.string_to_number('5.5') == 5
+    def test_apply_discount(self):
+        """
+        Тестирование метода apply_discount.
+        """
+        item = Item("Ноутбук", 20000, 5)
+        Item.pay_rate = 0.8
+        item.apply_discount(Item.pay_rate)
+
+        assert item.price == 16000.0
+
+    def test_rounded_price_after_discount(self):
+        """
+        Тестирование правильности округления стоимости товара после применения скидки.
+        """
+        item = Item("Компьютер", 15015.42, 3)
+        Item.pay_rate = 0.629
+        item.apply_discount(Item.pay_rate)
+
+        assert item.price == 9444.7
+
+    def test_all_items(self):
+        """
+        Тестирование правильности добавления и хранения созданных объектов класса Item.
+        """
+        item1 = Item("Смартфон", 10000, 20)
+        item2 = Item(" Ноутбук", 20000, 5)
+
+        assert len(Item.all_items) == 2
+        assert item1 in Item.all_items
+        assert item2 in Item.all_items
+
+    def test_name_setter(self):
+        item = Item('Телефон', 10000, 5)
+
+        # длина наименования товара меньше 10 символов
+        item.name = 'Смартфон'
+        assert item.name == 'Смартфон'
+
+        # длина наименования товара больше 10 символов
+        with pytest.raises(Exception) as excinfo:
+            item.name = 'СуперСмартфон'
+        assert "Длина наименования товара превышает 10 символов." in str(excinfo.value)
+
+    def test_instantiate_from_csv(self):
+        Item.instantiate_from_csv('src/items.csv')
+        assert len(Item.all) == 5
+
+    def test_string_to_number(self):
+        assert Item.string_to_number('5') == 5
+        assert Item.string_to_number('5.0') == 5
+        assert Item.string_to_number('5.5') == 5
